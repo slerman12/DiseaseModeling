82e94c2928454f9ad393b4926eb531858a686124
Sam Lerman <slerman@u.rochester.edu>
2016-06-24 14:28:15 -0400

    Obtained more metrics for Datscan model

--- Datscan.py
+++ Datscan.py
@@ -1,4 +1,5 @@
-from sklearn import preprocessing
+import warnings
+from sklearn import preprocessing, cross_validation
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.linear_model import LogisticRegression
@@ -6,7 +7,6 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
-from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
 
@@ -17,7 +17,7 @@ def main():
     # Create the training & test sets from files
     train = pd.read_csv("data/all_visits_practice.csv")
 
-    # Diagnostics
+    # Preliminary data diagnostics
     # print("Info")
     # print(train.info())
     # print("Describe")
@@ -34,7 +34,6 @@ def main():
 
     # Generate new features
     # new_features(train)
-    # new_features(test)
 
     # The columns we'll use to predict the target
     predictors = ["PATNO", "EVENT_ID", "CAUDATE_R", "CAUDATE_L", "PUTAMEN_R", "PUTAMEN_L"]
@@ -52,18 +51,8 @@ def main():
     # Fit the algorithm to the data
     rf.fit(train_predictors, train_target)
 
-    # # Predict
-    # predictions = rf.predict(test[predictors])
-    #
-    # # Create submission and output
-    # submission = pd.DataFrame({
-    #     "PassengerId": test["PassengerId"],
-    #     "Survived": predictions
-    # })
-    # submission.to_csv("data/kaggle.csv", index=False)
-
     # Metrics:
-    print("Random Forest Metrics: \n")
+    print("RANDOM FOREST METRICS: \n")
 
     # Perform feature selection
     selector = SelectKBest(f_classif, k='all')
@@ -85,55 +74,48 @@ def main():
     print(rf.score(train_predictors, train_target))
 
     # Cross validate our RF and output the mean score
-    # scores = cross_validation.cross_val_score(rf, train_predictors, train_target, cv=3)
-    # print("Cross validated score: ")
-    # print(scores.mean())
+    # with warnings.catch_warnings():
+    #     warnings.simplefilter("ignore")
+    #     scores = cross_validation.cross_val_score(rf, train_predictors, train_target, cv=3)
+    #     print("Cross validated score: ")
+    #     print(scores.mean())
 
     # Out of bag estimate
     print("OOB score: ")
     print(rf.oob_score_)
 
-    # Output roc auc score
-    # train['is_train'] = np.random.uniform(0, 1, len(train)) <= .75
-    # traindata, validatedata = train[train['is_train']==True], train[train['is_train']==False]
-    # x_traindata = traindata[predictors]
-    # y_traindata = traindata["NP3BRADY"]
-    # x_validatedata = validatedata[predictors]
-    # y_validatedata = validatedata["NP3BRADY"]
-    # rf.fit(x_traindata, y_traindata)
-    # disbursed = rf.predict_proba(x_validatedata)
-    # fpr, tpr, _ = roc_curve(y_validatedata, disbursed[:,1])
-    # roc_auc = auc(fpr, tpr)
-    # print("Roc_auc score (I don't fully understand this metric): ")
-    # print(roc_auc)
-
     # Split the data into a training set and a test set
-    X_train, X_test, y_train, y_test = train_test_split(train_predictors, train_target)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(train_predictors, train_target)
 
     # Print a confusion matrix for random forest
     y_pred_rf = rf.fit(X_train, y_train).predict(X_test)
-    print("\nConfusion matrix (rows: true, cols: pred)")
+    print("\nConfusion matrix of random forest (rows: true, cols: pred)")
     print(confusion_matrix(y_test, y_pred_rf))
 
     # Classification report
-    # print("\nClassification report:")
-    # print(classification_report(y_test, y_pred))
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        print("\nClassification report of random forest:")
+        print(classification_report(y_test, y_pred_rf))
 
-    # Accuracy scores of ensemble of Random Forest, Logistic Regression, and SVM
-    ensemble(rf, X_train, y_train, X_test, y_test, True, False)
+    print("ENSEMBLE METRICS:\n")
 
     # Base score of weighted ensemble of RF, LR, and SVM
-    ensemble(rf, train_predictors, train_target, train_predictors, train_target, False, True)
+    ensemble(rf, train_predictors, train_target, train_predictors, train_target, False, True, False, False)
+
+    # Accuracy scores of ensemble of Random Forest, Logistic Regression, and SVM
+    ensemble(rf, X_train, y_train, X_test, y_test, True, False, True, True)
 
 
 # Generate new features
 # def new_features(data):
 
 
-def ensemble(rf, X_train, y_train, X_test, y_test, accuracy, base_score):
+def ensemble(rf, X_train, y_train, X_test, y_test, accuracy, base_score, class_report, conf_mat):
     # Random forest
     y_pred_rf = rf.fit(X_train, y_train).predict(X_test)
-    if accuracy: print("\nAccuracy score of random forest: " + str(accuracy_score(y_test, y_pred_rf)))
+    if accuracy:
+        print("Accuracy score of random forest: " + str(accuracy_score(y_test, y_pred_rf)))
 
     # Logistic regression
     lr = LogisticRegression()
@@ -144,7 +126,8 @@ def ensemble(rf, X_train, y_train, X_test, y_test, accuracy, base_score):
     # SVM
     svm = SVC()
     y_pred_svm = svm.fit(X_train, y_train).predict(X_test)
-    if accuracy: print("Accuracy score of SVM: " + str(accuracy_score(y_test, y_pred_svm)))
+    if accuracy:
+        print("Accuracy score of SVM: " + str(accuracy_score(y_test, y_pred_svm)))
 
     # Decision tree
     dt = DecisionTreeClassifier()
@@ -155,11 +138,24 @@ def ensemble(rf, X_train, y_train, X_test, y_test, accuracy, base_score):
     # Weighted ensemble of RF, LR, and SVM
     y_pred_ensemble = ((y_pred_rf + y_pred_lr + y_pred_svm) / 3).astype(int)
     if accuracy:
-        print("Accuracy score of weighted ensemble of RF, LR, and SVM: " + str(accuracy_score(y_test, y_pred_ensemble)))
+        print("Accuracy score of ensemble of RF, LR, and SVM: " + str(accuracy_score(y_test, y_pred_ensemble)))
 
     # Base score
     if base_score:
-        print("Base score of weighted ensemble of RF, LR, and SVM: " +str(accuracy_score(y_train, y_pred_ensemble)))
+        print("Base score of ensemble of RF, LR, and SVM: " +str(accuracy_score(y_train, y_pred_ensemble)))
+
+    # Confusion matrix
+    if conf_mat:
+        print("\nConfusion matrix of ensemble (rows: true, cols: pred)")
+        print(confusion_matrix(y_test, y_pred_ensemble))
+        print("")
+
+    # Classification report
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        if class_report:
+            print("\nClassification report of ensemble of RF, LR, and SVM: ")
+            print(classification_report(y_test, y_pred_ensemble))
 
     return y_pred_ensemble
 
