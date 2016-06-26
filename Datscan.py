d85708d1370e8ebcf3eee92c728a82a57af09d44
Sam Lerman <slerman@u.rochester.edu>
2016-06-26 13:05:27 -0400

    Confusion matrix plot, more metrics

--- Datscan.py
+++ Datscan.py
@@ -10,6 +10,8 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -20,16 +22,16 @@ def main():
     train = pd.read_csv("data/all_visits_practice.csv")
 
     # Preliminary data diagnostics
-    # print("Info")
-    # print(train.info())
-    # print("Describe")
-    # print(train.describe())
-    # print("Unique")
+    print("PRELIMINARY DATA DIAGNOSTICS:\n")
+    print("Info:")
+    print(train.info())
+    print("\nDescribe:")
+    print(train.describe())
+    # print("Unique EVENT_ID")
     # print(train["EVENT_ID"].unique())
-    # print("Value counts EVENT_ID")
-    # print(pd.value_counts(train["EVENT_ID"]))
-    # print("Value counts NP3BRADY")
-    # print(pd.value_counts(train["NP3BRADY"]))
+    print("\nValue counts NP3BRADY:")
+    print(pd.value_counts(train["NP3BRADY"]))
+    print("")
 
     # Encode EVENT_ID to numeric
     train["EVENT_ID"] = preprocessing.LabelEncoder().fit_transform(train["EVENT_ID"])
@@ -78,91 +80,13 @@ def main():
     print("\nBase score: ")
     print(rf.score(train_predictors, train_target))
 
-    # Cross validate our RF and output the mean score
-    # with warnings.catch_warnings():
-    #     warnings.simplefilter("ignore")
-    #     scores = cross_validation.cross_val_score(rf, train_predictors, train_target, cv=3)
-    #     print("Cross validated score: ")
-    #     print(scores.mean())
-
     # Out of bag estimate
     print("OOB score: ")
     print(rf.oob_score_)
 
-    # # Split the data into a training set and a test set
-    # X_train, X_test, y_train, y_test = cross_validation.train_test_split(train_predictors, train_target)
-    #
-    # # Print a confusion matrix for random forest
-    # y_pred_rf = rf.fit(X_train, y_train).predict(X_test)
-    # print("\nConfusion matrix of random forest (rows: true, cols: pred)")
-    # print(confusion_matrix(y_test, y_pred_rf))
-    #
-    # # Classification report
-    # with warnings.catch_warnings():
-    #     warnings.simplefilter("ignore")
-    #     print("\nClassification report of random forest:")
-    #     print(classification_report(y_test, y_pred_rf))
-
     # Ensemble metrics
     print("\nENSEMBLE METRICS:\n")
     ensemble(rf, train_predictors, train_target)
-
-    # # Base score of weighted ensemble of RF, LR, and SVM
-    # ensemble(rf, train_predictors, train_target, train_predictors, train_target, False, True, False, False)
-    #
-    # # Accuracy scores of ensemble of Random Forest, Logistic Regression, and SVM
-    # ensemble(rf, X_train, y_train, X_test, y_test, True, False, True, True)
-
-
-# Generate new features
-# def new_features(data):
-
-
-# def ensemble(rf, X_train, y_train, X_test, y_test, accuracy, base_score, class_report, conf_mat):
-    # # Random forest
-    # y_pred_rf = rf.fit(X_train, y_train).predict(X_test)
-    # if accuracy:
-    #     print("Accuracy score of random forest: " + str(accuracy_score(y_test, y_pred_rf)))
-    #
-    # # Logistic regression
-    # lr = LogisticRegression()
-    # y_pred_lr = lr.fit(X_train, y_train).predict(X_test)
-    # if accuracy:
-    #     print("Accuracy score of logistic regression: " + str(accuracy_score(y_test, y_pred_lr)))
-    #
-    # # SVM
-    # svm = SVC()
-    # y_pred_svm = svm.fit(X_train, y_train).predict(X_test)
-    # if accuracy:
-    #     print("Accuracy score of SVM: " + str(accuracy_score(y_test, y_pred_svm)))
-    #
-    # # Decision tree
-    # dt = DecisionTreeClassifier()
-    # y_pred_dt = dt.fit(X_train, y_train).predict(X_test)
-    # if accuracy:
-    #     print("Accuracy score of decision tree: " + str(accuracy_score(y_test, y_pred_dt)))
-    #
-    # # Weighted ensemble of RF, LR, and SVM
-    # y_pred_ensemble = np.rint((y_pred_rf + y_pred_lr + y_pred_svm) / 3)
-    # if accuracy:
-    #     print("Accuracy score of ensemble of RF, LR, and SVM: " + str(accuracy_score(y_test, y_pred_ensemble)))# # Base score
-    # if base_score:
-    #     print("Base score of ensemble of RF, LR, and SVM: " +str(accuracy_score(y_train, y_pred_ensemble)))
-    #
-    # # Confusion matrix
-    # if conf_mat:
-    #     print("\nConfusion matrix of ensemble (rows: true, cols: pred)")
-    #     print(confusion_matrix(y_test, y_pred_ensemble))
-    #     print("")
-    #
-    # # Classification report
-    # with warnings.catch_warnings():
-    #     warnings.simplefilter("ignore")
-    #     if class_report:
-    #         print("\nClassification report of ensemble of RF, LR, and SVM: ")
-    #         print(classification_report(y_test, y_pred_ensemble))
-    #
-    # return y_pred_ensemble
 
 
 def ensemble(rf, X, y):
@@ -171,32 +95,58 @@ def ensemble(rf, X, y):
     svm = SVC(probability=True)
     gnb = GaussianNB()
     knn = KNeighborsClassifier(n_neighbors=7)
+    gb = GradientBoostingClassifier(n_estimators=350)
 
     # Ensemble
+    # eclf = VotingClassifier(estimators=[('rf', rf), ('lr', lr), ('svm', svm), ('gnb', gnb), ('knn', knn), ('gb', gb)], voting='soft')
     eclf = VotingClassifier(estimators=[('rf', rf), ('lr', lr), ('svm', svm)], voting='soft')
 
+    # Cross validation accuracies
     print("Cross validation:\n")
-    # Accuracies
-    for clf, label in zip([rf, lr, svm, gnb, knn, eclf], ['Random Forest', 'Logistic Regression', 'SVM', 'naive Bayes', 'kNN', 'Ensemble of RF, LR, and SVM']):
-        # fit_params = {"sample_weight", [2, 1, 3, 3, 4, 4]}
+    for clf, label in zip([rf, lr, svm, gnb, knn, gb, eclf], ['Random Forest', 'Logistic Regression', 'SVM', 'naive Bayes', 'kNN', 'Gradient Boosting', 'Ensemble of RF, LR, and SVM']):
         scores = cross_validation.cross_val_score(clf, X, y, cv=2, scoring='accuracy')
         print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
 
     # Split the data into a training set and a test set
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25)
-
     print("\n75/25 split: \n")
-
-    # Print a confusion matrix for ensemble
     y_pred_ensemble = eclf.fit(X_train, y_train).predict(X_test)
-    print("Confusion matrix of ensemble (rows: true, cols: pred)")
-    print(confusion_matrix(y_test, y_pred_ensemble))
-    print("")
 
     # Accuracies
-    for clf, label in zip([rf, lr, svm, gnb, knn, eclf], ['Random Forest', 'Logistic Regression', 'SVM', 'naive Bayes', 'kNN', 'Ensemble of RF, LR, and SVM']):
+    for clf, label in zip([rf, lr, svm, gnb, knn, gb, eclf], ['Random Forest', 'Logistic Regression', 'SVM', 'naive Bayes', 'kNN', 'Gradient Boosting', 'Ensemble of RF, LR, and SVM']):
         y_pred = clf.fit(X_train, y_train).predict(X_test)
         print("Accuracy: %0.2f [%s]" % (accuracy_score(y_test, y_pred), label))
+
+    # Print a confusion matrix for ensemble
+    print("\nConfusion matrix of ensemble (rows: true, cols: pred)")
+
+    def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(4)
+        plt.xticks(tick_marks, [0, 1, 2, 3], rotation=45)
+        plt.yticks(tick_marks, [0, 1, 2, 3])
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_test, y_pred_ensemble)
+    np.set_printoptions(precision=2)
+    print('Confusion matrix, without normalization:')
+    print(cm)
+
+    # Normalize the confusion matrix by row (i.e by the number of samples in each class)
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print('Normalized confusion matrix:')
+    print(cm_normalized)
+    plt.figure()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        plot_confusion_matrix(cm_normalized, title='Normalized confusion matrix')
+
+    plt.show()
 
 
 if __name__ == "__main__":
