9c167e5b157e096a238ff6132d6bb503430d4f22
Sam Lerman <slerman@u.rochester.edu>
2016-07-06 22:42:38 -0400

    Added to AllHypertension

--- AllHypertension.py
+++ AllHypertension.py
@@ -7,7 +7,7 @@ def main():
     # Set seed
     np.random.seed(0)
 
-    # Create the training/test set(s) from file(s)
+    # Create the dataframe from file
     data = pd.read_csv("data/all_bp.csv")
 
     # Set dawn and dusk constants
@@ -17,21 +17,36 @@ def main():
     # Convert datetimes to pandas datetimes
     data["date_time_local"] = pd.to_datetime(data["date_time_local"])
 
-    # Find first previous dawn before a time
-    def find_previous_dawn(date_time):
-        # Today's dawn
-        today_dawn = pd.Timestamp(date_time.date + " " + dawn)
+    # Find previous dawn before a time
+    def find_previous_dawn(first_date_time, last_date_time):
+        # First day's dawn and last day's dawn
+        first_dawn = pd.Timestamp(first_date_time.date + " " + dawn)
+        last_dawn = pd.Timestamp(last_date_time.date + " " + dawn)
 
-        # Return first previous dawn
-        if date_time >= today_dawn:
-            return today_dawn
-        else:
-            return today_dawn - Day(1)
+        # Previous dawn
+        if first_date_time < first_dawn:
+            first_dawn = first_dawn - Day(1)
+
+        # Next dawn
+        if last_date_time > last_dawn:
+            last_dawn = last_dawn + Day(1)
+
+        # Return first and last dawn
+        return first_dawn, last_dawn
 
     # Iterate through each patient
     for patient in data["id"].unique():
         # Initialize time as first dawn before earliest observation
-        time = find_previous_dawn(data.loc[data["id"] == patient, "date_time_local"].min())
+        time, last_time = find_previous_dawn(data.loc[data["id"] == patient, "date_time_local"].min())
+
+        # Iterate by 24 hour periods from dawn to dawn
+        while time != last_time:
+            # Observations of this patient on this time interval
+            observations = data[data["id"] == patient & data["date_time_local"].between(time, time + Day(1))]
+
+            # Iterate by a day
+            time = time + Day(1)
+
 
 
 if __name__ == "__main__":
